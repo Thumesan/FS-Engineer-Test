064e2ff336c831b4a54a9c112760eaaf5bb5e257
Thumesan Moodley <thumesan.moodley@icloud.com>
2022-02-24 23:15:30 +0200

    Completed Backend Without CORS functioning

---
+++ FS_Engineer_Test/FS_Engineer_Test/Models/SwapiPeople.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace FS_Engineer_Test.Models
+{
+    public class SwapiPeople
+    {
+        public int Count { get; set; }
+        public string Next { get; set; }
+        public string Previous { get; set; }
+        public IEnumerable<SwapiPerson> Results { get; set; }
+
+    }
+}
