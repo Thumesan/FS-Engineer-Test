179484637874a5f5909db8cecbc68adcb9403049
Thumesan Moodley <thumesan.moodley@icloud.com>
2022-02-26 23:29:15 +0200

    Changed docker file and added heroku backend path

--- FS-Engineer-Test-FE/src/app/api/api-configuration.ts
+++ FS-Engineer-Test-FE/src/app/api/api-configuration.ts
@@ -9,7 +9,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class ApiConfiguration {
-  rootUrl: string = '/api';
+  rootUrl: string = 'https://fs-engineer-test.herokuapp.com';
 }
 
 /**
